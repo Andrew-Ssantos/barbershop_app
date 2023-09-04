8018188b7a6483124e63ac2cf7e9b9cdb6d4fa44
Andrew-Ssantos <andrew-s-silva@outlook.com>
2023-09-04 17:52:46 -0300

    Iniciando aula 4 DW 11

---
+++ dw_barbershop/lib/src/features/home/widgets/home_header.dart
@@ -0,0 +1,105 @@
+import 'package:dw_barbershop/src/core/ui/barbershop_icons.dart';
+import 'package:dw_barbershop/src/core/ui/constants.dart';
+import 'package:flutter/material.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      width: MediaQuery.sizeOf(context).width,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(32),
+          bottomRight: Radius.circular(32),
+        ),
+        color: Colors.black,
+        image: DecorationImage(
+          image: AssetImage(
+            ImageConstants.backgroundChair,
+          ),
+          fit: BoxFit.cover,
+          opacity: 0.5,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                backgroundColor: Color(0xffbdbdbd),
+                child: SizedBox.shrink(),
+              ),
+              const SizedBox(width: 16),
+              const Flexible(
+                child: Text(
+                  'Andrew dos Santos Silva gosta de paçoca',
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              const Expanded(
+                child: Text(
+                  'Editar',
+                  style: TextStyle(
+                    color: ColorsConstants.brow,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  BarbershopIcons.exit,
+                  color: ColorsConstants.brow,
+                  size: 32,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 24),
+          const Text(
+            'Bem Vindo',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 24),
+          const Text(
+            'Agende um Cliente',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 40,
+            ),
+          ),
+          const SizedBox(height: 24),
+          TextFormField(
+            decoration: const InputDecoration(
+              label: Text('Buscar Colaborador'),
+              suffixIcon: Padding(
+                padding: EdgeInsets.only(right: 24),
+                child: Icon(
+                  BarbershopIcons.search,
+                  color: ColorsConstants.brow,
+                  size: 26,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
